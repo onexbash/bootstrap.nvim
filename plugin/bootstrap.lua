4e3bc6f903774f18890c8ba2c71462e6391fd842
fschlegelone <fabian@schlegel.one>
2024-10-28 13:20:23 +0100

    fix: nvim editorcommand

--- plugin/bootstrap.lua
+++ plugin/bootstrap.lua
@@ -39,7 +39,7 @@ function M.show()
 end
 
 -- Create the :1bootstrap command
-vim.api.nvim_create_user_command("1bootstrap", function()
+vim.api.nvim_create_user_command("bootstrap", function()
 	M.show()
 end, {})
 
